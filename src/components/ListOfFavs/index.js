3d7aebf18fea0ac181bbf1a61292bb0e4e0d6349
Alberto Flores <alberto.floreso94@gmail.com>
2021-04-07 16:30:34 -0500

    add PropTypes

--- src/components/ListOfFavs/index.js
+++ src/components/ListOfFavs/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid, Image, Link } from './styles'
+import PropTypes from 'prop-types'
 
 export const ListOfFavs = ({ favs = [] }) => {
     return (
@@ -14,5 +15,14 @@ export const ListOfFavs = ({ favs = [] }) => {
                 }
             </Grid>
         </>
+    )
+}
+
+ListOfFavs.propTypes = {
+    favs: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string,
+            src: PropTypes.string
+        })
     )
 }
